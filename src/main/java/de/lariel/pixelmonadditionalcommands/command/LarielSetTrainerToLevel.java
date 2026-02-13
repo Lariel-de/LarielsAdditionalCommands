88fbcc1bcf29314c1375ff3f3cede56ec3b8914c
Lariel <nadine.hoffmann1989@outlook.com>
2026-02-13 13:52:55 +0100

    Let the user also set the trainer to equal level with LarielSetTrainerToLevel Command

--- src/main/java/de/lariel/pixelmonadditionalcommands/command/LarielSetTrainerToLevel.java
+++ src/main/java/de/lariel/pixelmonadditionalcommands/command/LarielSetTrainerToLevel.java
@@ -3,8 +3,10 @@ package de.lariel.pixelmonadditionalcommands.command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.pixelmonmod.pixelmon.api.command.PixelmonCommandUtils;
 import com.pixelmonmod.pixelmon.api.config.PixelmonConfigProxy;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
 import com.pixelmonmod.pixelmon.api.storage.PartyStorage;
 import com.pixelmonmod.pixelmon.api.storage.StorageProxy;
 import com.pixelmonmod.pixelmon.battles.BattleRegistry;
@@ -15,6 +17,7 @@ import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.UnknownNullability;
 
 import java.util.Optional;
 
@@ -26,6 +29,8 @@ public class LarielSetTrainerToLevel {
                                 .suggests((ctx, builder) -> {
                                     var max = PixelmonConfigProxy.getGeneral().getMaxLevel();
 
+                                    builder.suggest("equal");
+
                                     // Fix sorting of the recommended levels
                                     for (var i = 1; i <= max; i++) {
                                         builder.suggest(i, Component.literal(String.valueOf(i)));
@@ -34,10 +39,9 @@ public class LarielSetTrainerToLevel {
                                     return builder.buildFuture();
                                 }).executes(ctx -> {
                                     var target = EntityArgument.getEntity(ctx, "target");
-                                    var lvl = StringArgumentType.getString(ctx, "level");
-                                    var level = PixelmonCommandUtils.requireInt(lvl, 1, PixelmonConfigProxy.getGeneral().getMaxLevel(), "parsing.int.invalid");
+                                    var level = GetTargetLevel(ctx);
 
-                                    if (!verifyEntity(ctx, target)) {
+                                    if (!VerifyEntity(ctx, target)) {
                                         LarielErrorLog.LogError("Entity could not be edited", ctx);
                                     }
 
@@ -51,7 +55,11 @@ public class LarielSetTrainerToLevel {
                                             .orElse(null);
 
                                     if (team == null) {
-                                        return LarielErrorLog.LogError("Couldn't get team from entity", ctx);
+                                        var npcName = Optional.ofNullable(npc.getCustomName())
+                                                .map(Component::getString)
+                                                .orElse("Couldn't read NPC name.");
+
+                                        return LarielErrorLog.LogError("Couldn't get team from entity: " + npcName, ctx);
                                     }
 
                                     team.forEach((p) -> p.setLevel(level));
@@ -61,7 +69,34 @@ public class LarielSetTrainerToLevel {
                                 }))));
     }
 
-    private static boolean verifyEntity(CommandContext<CommandSourceStack> commandContext, Entity entity) {
+    private static int GetTargetLevel(@UnknownNullability CommandContext<CommandSourceStack> ctx) throws CommandSyntaxException {
+        var lvl = StringArgumentType.getString(ctx, "level");
+
+        // Executor has entered a static number.
+        if (!lvl.equals("equal")) {
+            // Let the trainers Pokémon up to lvl 1000
+            return PixelmonCommandUtils.requireInt(lvl, 1, 1000, "parsing.int.invalid");
+        }
+
+        var player = ctx.getSource().getPlayer();
+
+        if (player == null) {
+            return LarielErrorLog.LogDebug("\"equal\"-level is just allowed if command is executed by players.", ctx);
+        }
+
+        var party = StorageProxy.getPartyNow(player.getUUID());
+        var team = Optional.ofNullable(party)
+                .map(PartyStorage::getTeam)
+                .orElse(null);
+
+        if (team == null) {
+            return LarielErrorLog.LogError("Couldn't read your team", ctx);
+        }
+
+        return team.stream().mapToInt(Pokemon::getPokemonLevel).max().orElse(1);
+    }
+
+    private static boolean VerifyEntity(CommandContext<CommandSourceStack> commandContext, Entity entity) {
         // If entity is in battle
         if (BattleRegistry.getBattle(entity) != null) {
             commandContext.getSource().sendFailure(Component.translatable("pixelmon.command.battle.cannotchallenge", entity.getDisplayName().getString()));
